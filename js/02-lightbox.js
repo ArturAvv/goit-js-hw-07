3b936551a91395f61e7aff2ccd4427828bb62755
ArturAvv <avvakumovart@gmail.com>
2022-10-21 20:09:03 +0300

    finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const getGallery = document.querySelector(".gallery");
+
+const createGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+  })
+  .join("");
+
+getGallery.insertAdjacentHTML("beforeend", createGallery);
+
+var lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
